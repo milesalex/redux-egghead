eb0324f81a47c26384e516ee3326a4e991bb9d84
Alex Miles <milesalex@users.noreply.github.com>
2017-08-16 09:36:31 -0400

    Init

---
+++ src/components/TodoForm.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { updateCurrent } from '../reducers/todo'
+
+const TodoForm = (props) => {
+  const {currentTodo, updateCurrent} = props
+  const handleInputChange = (e) => {
+    const val = e.target.value
+    updateCurrent(val)
+  }
+
+  return (
+    <form>
+      <input
+        type="text"
+        onChange={handleInputChange}
+        value={currentTodo} />
+    </form>
+  )
+}
+
+export default connect(
+  (state) => ({currentTodo: state.currentTodo}),
+  {updateCurrent}
+)(TodoForm)
---
+++ src/reducers/todo.js
@@ -0,0 +1,32 @@
+import {getTodos} from'../lib/todoServices'
+const initState = {
+  todos: [],
+  currentTodo: ''
+}
+
+const TODO_ADD = 'TODO_ADD'
+const TODOS_LOAD = 'TODOS_LOAD'
+const CURRENT_UPDATE = 'CURRENT_UPDATE'
+
+export const updateCurrent = (val) => ({type: CURRENT_UPDATE, payload: val})
+export const loadTodos = (todos) => ({type: TODOS_LOAD, payload: todos})
+export const fetchTodos = () => {
+  return (dispatch) => {
+    getTodos()
+    .then(todos => dispatch(loadTodos(todos)))
+  }
+}
+
+
+export default (state = initState, action) => {
+  switch (action.type) {
+    case TODO_ADD:
+      return {...state, todos: state.todos.concat(action.payload)}
+    case TODOS_LOAD:
+      return {...state, todos: action.payload}
+    case CURRENT_UPDATE:
+      return {...state, currentTodo: action.payload}
+    default:
+      return state
+  }
+}
